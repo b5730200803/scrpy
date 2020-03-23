512f0bbcdb410ddc02fcfd622e4be82789896536
MiniPair <supakorn1497@gmail.com>
2020-03-23 21:46:13 +0700

    Update

--- scrapy_simple/pythonfile/sentiment3.py
+++ scrapy_simple/pythonfile/sentiment3.py
@@ -16,11 +16,11 @@ import itertools
 import os.path
 import pandas as pd
 
-keyword = [];
-increase_key = [];
-decrease_key = [];
-dates = [];
-w,h = 0,0
+keyword = []
+increase_key = []
+decrease_key = []
+dates = []
+w, h = 0, 0
 news = [[0 for x in range(w)] for y in range(h)]
 table = [[0 for x in range(w)] for y in range(h)]
 
@@ -29,105 +29,122 @@ now = datetime.now()
 dateTime = now.strftime("%Y-%m-%d %H.%M.%S")
 #################################################################################################
 #################################################################################################
+
+
 def invertTable(trend):
+
     if(trend == 1):
         present_key = increase_key
-        pre_word = "increase";
+        pre_word = "increase"
     else:
         present_key = decrease_key
-        pre_word = "decrease";
+        pre_word = "decrease"
 
-    data = {};
-    header2 = [dates[0]];
-    data[dates[0]] = [];
+    # สร้าง object data เพื่อเก็บ array ข้อมูล
+    data = {}
+    # header index ที่ 0 เก็บค่าตาราง datas index ที่ 0 ไว้ มีคำว่า 'date'
+    header = [dates[0]]
+    data[dates[0]] = []
 
+    for i in range(1, len(present_key)):
+        header.append(present_key[i])
+        data[header[i]] = []
+        for j in range(1, len(dates)):
+            data[header[i]].append(0)
 
-    for i in range(len(present_key)):
-        header2.append(present_key[i]);
-        data[header2[i]] = []
-   
-    for i in range(len(data)):
-        print(data)
+    for j in range(1, len(table[0])):
+        data[dates[0]].append(dates[j])
+        # for i in range(len(table)):
+        #     if table[i][0] not in present_key:
+        #         continue
+        #     else:
+        #         data[table[i][0]].append(table[i][j])
 
-    # for i in range(len(header2)):
-    #     print(header2[i])
+    # for i in range (0,len(data)):
+    #    print(header[i])
 
-    # for j in range(1,len(table[0])):
-    #     data[dates[0]].append(dates[j])
-    #     for i in range (len(table)):
-    #         data[table[i][0]].append(table[i][j])
-    
-    # excel = json.dumps(data)
+    for i in range(len(table)):
+        data[table[i][0]] = table[i][1:]
 
-    # df =  pd.DataFrame(data,columns=data.keys())
-    # export_excel = df.to_excel (r'C:\Users\supak\Desktop\scrpy\scrapy_simple\result\export_'+pre_word+'_'+dateTime+'.xlsx', index = None, header=True)
+    excel = json.dumps(data)
+    df = pd.DataFrame(data, columns=data.keys())
+    export_excel = df.to_excel(r'C:\Users\MiniPair\Desktop\scrpy\scrapy_simple\result\export_' +
+                               pre_word+'_'+dateTime+'.xlsx', index=None, header=True)
 
 #################################################################################################
 #################################################################################################
-def insert_table(word,indexDate):
-       
-    for i in range (len(table)):
+
+
+def insert_table(word, indexDate):
+
+    for i in range(len(table)):
         if word == table[i][0]:
-            table[i][indexDate] = 1 
-    
+            table[i][indexDate] = 1
 
-    zeros = [];
-    zeros = zeros + [0]*(len(dates)-1 - len(zeros));
-    table.insert(len(table),[word]+zeros);
-    table[len(table)-1][indexDate] += 1;
+    zeros = []
+    zeros = zeros + [0]*(len(dates)-1 - len(zeros))
+    table.insert(len(table), [word]+zeros)
+    table[len(table)-1][indexDate] += 1
 
     return True
 #################################################################################################
 #################################################################################################
+
+
 def readFileDictionary(file):
 
     wd = xlrd.open_workbook(file)
     sheet = wd.sheet_by_index(0)
-    sheet.cell_value(0,0)
+    sheet.cell_value(0, 0)
 
-    for i in range(1,sheet.nrows):
-        if(sheet.cell_value(i,1) != ''):
-            temp = sheet.cell_value(i,1).lower().split(' ');
-            keyword.insert(i,temp);
-    
-    for i in range(1,sheet.nrows):
-        if(sheet.cell_value(i,2) != ''):
-            increase_key.insert(i,sheet.cell_value(i,2).lower());
+    for i in range(1, sheet.nrows):
+        if(sheet.cell_value(i, 1) != ''):
+            temp = sheet.cell_value(i, 1).lower().split(' ')
+            keyword.insert(i, temp)
 
-    for i in range(1,sheet.nrows):
-        if(sheet.cell_value(i,3) != ''):
-            decrease_key.insert(i,sheet.cell_value(i,3).lower());
+    for i in range(1, sheet.nrows):
+        if(sheet.cell_value(i, 2) != ''):
+            increase_key.insert(i, sheet.cell_value(i, 2).lower())
+
+    for i in range(1, sheet.nrows):
+        if(sheet.cell_value(i, 3) != ''):
+            decrease_key.insert(i, sheet.cell_value(i, 3).lower())
     return True
 #################################################################################################
 #################################################################################################
+
+
 def readFileNews():
     with open(os.path.abspath('virtual_env\demo_project\\news.json')) as filenews:
         sheet = json.load(filenews)
-    
-    for i in range (len(sheet['title'])):
-        tmp_title = sheet['title'][i].lower();
-        tmp_content = sheet['content'][i].lower();
-        tmp_date = sheet['date'][i].split(" ",1)[0];
-        
-        f3 = open("file\\apostrophe-lower.txt","r")
+
+    for i in range(len(sheet['title'])):
+        tmp_title = sheet['title'][i].lower()
+        tmp_content = sheet['content'][i].lower()
+        tmp_date = sheet['date'][i].split(" ", 1)[0]
+
+        f3 = open("file\\apostrophe-lower.txt", "r")
         for line3 in f3:
             words3 = line3.split(",")
-            tmp_title = tmp_title.replace(words3[1].strip().lower(),words3[0].lower())
-            tmp_content = tmp_content.replace(words3[1].strip().lower(),words3[0].lower())
-        
-        news.insert(i-1,[tmp_title,tmp_content,tmp_date]);
-        
+            tmp_title = tmp_title.replace(
+                words3[1].strip().lower(), words3[0].lower())
+            tmp_content = tmp_content.replace(
+                words3[1].strip().lower(), words3[0].lower())
 
-    news.sort(key=lambda x: x[2]);
+        news.insert(i-1, [tmp_title, tmp_content, tmp_date])
+
+    news.sort(key=lambda x: x[2])
 
     for i in range(len(news)):
-        dates.append(news[i][2]);  
+        dates.append(news[i][2])
 
-    dates.insert(0,'date')
+    dates.insert(0, 'date')
     return True
 #################################################################################################
 #################################################################################################
-def findWord(sentence,word):
+
+
+def findWord(sentence, word):
     if sentence.find(' '+word) == -1 and sentence.find(' '+word+' ') == -1 and sentence.find(word+' ') == -1:
         return -1
     elif sentence.find(' '+word+' ') != -1 or sentence.find(word+' ') == 0 or sentence.find(' '+word)+len(word) == len(sentence):
@@ -137,65 +154,71 @@ def findWord(sentence,word):
 
 #################################################################################################
 #################################################################################################
+
+
 def cutKum(trend):
     if trend == 1:
-        present_key = increase_key;
+        present_key = increase_key
     else:
-        present_key = decrease_key;
+        present_key = decrease_key
 
-    count = 0 
+    count = 0
 
     # ทำการอ่านแต่ละข่าว
     for iNews in news:
-        count+=1;
-        index_date = dates.index(iNews[2]);
-        iNews[1] = iNews[1].replace('[\'','').replace('\']','').replace(', ','').replace('\'\'',' ');
-        
+        count += 1
+        index_date = dates.index(iNews[2])
+        iNews[1] = iNews[1].replace('[\'', '').replace(
+            '\']', '').replace(', ', '').replace('\'\'', ' ')
+
         # รวมหัวเรื่องข่าวกับเนื้อหาข่าวให้อยู่ใน content เดียวกัน
-        mixNews = iNews[0]+'. '+iNews[1];
-        content = nltk.sent_tokenize(mixNews);
-        tempKey = [];
+        mixNews = iNews[0]+'. '+iNews[1]
+        content = nltk.sent_tokenize(mixNews)
+        tempKey = []
 
         # วนอ่านแต่ละ sentense ใน content
         for sentense in content:
             # วนอ่่านแต่ละ key ใน พจนานุกรมแนวโน้มราคาน้ำมัน
-            # เรียกแทนว่า dialog 
+            # เรียกแทนว่า dialog
             for dialog in present_key:
-                isMatch = True;
+                isMatch = True
                 subSen = sentense
 
-                #กรณีมี dialog ที่เคยเจอแล้ว
-                #ให้ข้าม ไป ยัง dialog ถัดไป
+                # กรณีมี dialog ที่เคยเจอแล้ว
+                # ให้ข้าม ไป ยัง dialog ถัดไป
                 if dialog in tempKey:
                     continue
-                
+
                 # ทำการตัด dialog ให้อยู่ในรูปอาเรย์ ในชื่อตัวแปร subDialog
                 # เพื่อรองรับกรณี dialog ที่เป็นกลุ่มคำ เช่น turn on
-                subDialog = dialog.split(" ");
+                subDialog = dialog.split(" ")
 
                 for iWord in subDialog:
-                    
+
                     if findWord(subSen, iWord) == -1:
-                       isMatch = False
-                       break
+                        isMatch = False
+                        break
                     else:
-                        tempIndex = findWord(subSen,iWord);
-                        subSen = subSen[tempIndex:];
-                
+                        tempIndex = findWord(subSen, iWord)
+                        subSen = subSen[tempIndex:]
+
                 if isMatch:
-                    tempKey.append(dialog);
-                    insert_table(str(dialog),index_date);
+                    tempKey.append(dialog)
+                    insert_table(str(dialog), index_date)
 
     return True
 #################################################################################################
 #################################################################################################
+
+
 def main():
-    start = datetime.now();
-    dictionaryFile = "file\DictionaryOil_270220.xlsx";
-    readFileDictionary(dictionaryFile);
-    readFileNews();
+    start = datetime.now()
+    dictionaryFile = "file\DictionaryOil_270220.xlsx"
+    readFileDictionary(dictionaryFile)
+    readFileNews()
     cutKum(1)
     sorted(table, key=lambda t: t[0])
     invertTable(1)
+
 
 main()
