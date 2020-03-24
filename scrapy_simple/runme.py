d9615ed8bcaf6b524b41b45caca0da5ecd73e1dc
MiniPair <supakorn1497@gmail.com>
2020-03-24 17:18:18 +0700

    Delete file test and Analysed

---
+++ scrapy_simple/runme.py
@@ -0,0 +1,20 @@
+import time 
+import subprocess
+import json 
+import os.path
+import sys
+sys.path.insert(0,'/scrpy/scrapy_simple/pythonfile/')
+
+# # from pythonfile import merged
+
+# subprocess.run(["scrapy","crawl", "oilnews"])
+
+
+
+
+# # while True:
+    
+# #     time.sleep(10)
+
+
+    
